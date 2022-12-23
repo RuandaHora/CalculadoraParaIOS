2d33f57d528b6b388868911702ddb5d8cdf80836
Ruan da Hora <ruanpablosnake@gmail.com>
2022-12-22 21:45:20 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Marcelo Hora on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
